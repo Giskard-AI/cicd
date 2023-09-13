2b656f06ceadee9f545fd73e5b251a8efea2f14d
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-09-13 12:39:10 +0200

    added scan_retrieved.py script for automation

---
+++ scan_retrieved.py
@@ -0,0 +1,47 @@
+import argparse
+import pandas as pd
+from ast import literal_eval
+from string import Template
+import os
+
+
+def model_has_dataset(model):
+    for tag in model.tags:
+        if tag.startswith("dataset:"):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Giskard Batch Scanner", description="Scan Retrieved HF models."
+    )
+    parser.add_argument(
+        "--data_path",
+        help="Path to retrieved models in csv format (need to run retrieve.py first).",
+        required=True,
+    )
+    parser.add_argument("--first_Nmodels",
+                        help="Number of models to be scanned from the sorted list of models available.",
+                        required=True)
+    parser.add_argument("--output_path",
+                        help="Path of dir to save all the reports",
+                        required=True)
+
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.data_path)
+
+    command_template = Template("python cli.py --loader huggingface --model $model --dataset $dataset "
+                                "--dataset_split validation --output $output_path/${model}__default_scan_with__${dataset}.html")
+
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+
+    for i in range(int(args.first_Nmodels)):
+        row = df.iloc[i]
+        model = row.modelId
+        dataset = literal_eval(row.datasets)[0]
+
+        command = command_template.substitute(model=model, dataset=dataset, output_path=args.output_path)
+        os.system(command)
