d26d56ebed5b33e7318831d2625a8e1eb4460a89
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-09-13 12:46:59 +0200

    added overwriting logic

--- scan_retrieved.py
+++ scan_retrieved.py
@@ -33,7 +33,9 @@ if __name__ == "__main__":
     df = pd.read_csv(args.data_path)
 
     command_template = Template("python cli.py --loader huggingface --model $model --dataset $dataset "
-                                "--dataset_split validation --output $output_path/${model}__default_scan_with__${dataset}.html")
+                                "--dataset_split validation --output ${output_path}/${model}__default_scan_with__${dataset}.html")
+
+    check_exist_template = Template("${output_path}/${model}__default_scan_with__${dataset}.html")
 
     if not os.path.exists(args.output_path):
         os.makedirs(args.output_path)
@@ -42,6 +44,18 @@ if __name__ == "__main__":
         row = df.iloc[i]
         model = row.modelId
         dataset = literal_eval(row.datasets)[0]
+
+        result_path = check_exist_template.substitute(model=model, dataset=dataset, output_path=args.output_path)
+        if os.path.exists(result_path):
+            answer = input(f"{result_path} already exists, Overwrite[o] or Skip[s]? ")
+
+            while answer not in ["o", "s"]:
+                answer = input("Unvalid answer, please choose between 'o' and 's'")
+
+            if answer == 'o':
+                os.remove(result_path)
+            elif answer == 's':
+                continue
 
         command = command_template.substitute(model=model, dataset=dataset, output_path=args.output_path)
         os.system(command)
