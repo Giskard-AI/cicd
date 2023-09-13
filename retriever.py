1fe67d0df324de0ddef61904bebb09afd4d42131
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2023-09-13 11:53:21 +0200

    enhanced retriever.py

--- retriever.py
+++ retriever.py
@@ -1,5 +1,4 @@
-# %%
-
+import argparse
 import huggingface_hub
 
 
@@ -10,31 +9,47 @@ def model_has_dataset(model):
     return False
 
 
-# %%
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Giskard Retriever", description="Retrieves HF models that are bound to datasets."
+    )
+    parser.add_argument(
+        "--model_type",
+        help="Hugging Face model types. default: text-classification",
+        required=False,
+    )
+    parser.add_argument("--output_format",
+                        help="Format of the information retrieved. Default: parquet. Options: parquet, csv, json.")
 
-MODEL_TYPE = "text-classification"
+    args = parser.parse_args()
 
-models_with_dataset = filter(
-    model_has_dataset, huggingface_hub.list_models(filter=MODEL_TYPE, sort="likes", direction=-1)
-)
+    MODEL_TYPE = args.model_type if args.model_type is not None else "text-classification"
 
+    models_with_dataset = filter(
+        model_has_dataset, huggingface_hub.list_models(filter=MODEL_TYPE, sort="likes", direction=-1)
+    )
 
-import pandas as pd
+    import pandas as pd
 
-df = pd.DataFrame(
-    [
-        {
-            "modelId": m.modelId,
-            "modelType": MODEL_TYPE,
-            "author": m.author,
-            "downloads": m.downloads,
-            "likes": m.likes,
-            "datasets": [t[8:] for t in m.tags if t.startswith("dataset:")],
-        }
-        for m in models_with_dataset
-    ]
-)
+    df = pd.DataFrame(
+        [
+            {
+                "modelId": m.modelId,
+                "modelType": MODEL_TYPE,
+                "author": m.author,
+                "downloads": m.downloads,
+                "likes": m.likes,
+                "datasets": [t[8:] for t in m.tags if t.startswith("dataset:")],
+            }
+            for m in models_with_dataset
+        ]
+    )
 
-df.to_parquet(f"models_{MODEL_TYPE}.parquet", index=False)
+    output_format = args.output_format
 
-# %%
+    if output_format is None or output_format == "parquet":
+        df.to_parquet(f"models_{MODEL_TYPE}.parquet", index=False)
+    elif output_format == "csv":
+        df.to_csv(f"models_{MODEL_TYPE}.csv", columns=df.columns, index=False)
+    elif output_format == "json":
+        df.to_json(f"models_{MODEL_TYPE}.json", index=False)
