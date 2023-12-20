5fa681596af684ca802c2ee6e1b784f76ea83562
zcy <congyao119@gmail.com>
2023-12-20 16:10:25 +0100

    avoid nonetype error

--- giskard_cicd/pipeline/runner.py
+++ giskard_cicd/pipeline/runner.py
@@ -33,8 +33,8 @@ class PipelineRunner:
         if scan_config_path is not None:
             with open(scan_config_path) as yaml_f:
                 scan_config = yaml.load(yaml_f, Loader=yaml.Loader)
-            params = dict(scan_config.get("configuration", {}))
-            detectors = list(scan_config.get("detectors"))
+            params = dict(scan_config.get("configuration") or dict())
+            detectors = list(scan_config.get("detectors") or [])
             kwargs.update({"inference_type": scan_config.get("inference_type", "hf_inference_api")})
 
         start = time.time()
