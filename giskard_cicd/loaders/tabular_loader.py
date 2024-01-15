09ae625a6a90b91129041c6f806506f32fe58482
zcy <congyao119@gmail.com>
2024-01-15 17:17:55 +0100

    handle errors for tabular

--- giskard_cicd/loaders/tabular_loader.py
+++ giskard_cicd/loaders/tabular_loader.py
@@ -1,11 +1,21 @@
-from transformers import Pipeline
+from transformers import Pipeline, AutoModel
+import huggingface_hub
+import joblib
+import os
 
 class TabularClassificationPipeline(Pipeline):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self._model_type = "tabular-classification"
         self._check_model_type(self._model_type)
         self.pipeline_tag = "tabular-classification"
+        # get model parameter from args
+        model_id = kwargs.pop("model", None)
+        self.model_dir = huggingface_hub.snapshot_download(model_id)
+        for f in os.listdir(self.model_dir):
+            if ".joblib" in f:
+                self.model = joblib.load(self.model_dir + "/" + f)
+            if ".pb" in f:
+                self.model = AutoModel.from_pretrained(self.model_dir)
 
     def _sanitize_parameters(self, **kwargs):
         kwargs = super()._sanitize_parameters(**kwargs)
@@ -18,10 +28,11 @@ class TabularClassificationPipeline(Pipeline):
             )
         
     def _forward(self, *args, **kwargs):
-        raise NotImplementedError("Not implemented yet")
+        return super()._forward(*args, **kwargs)
     
-    def preporocess(self, *args, **kwargs):
+    def preprocess(self, *args, **kwargs):
         return super().preprocess(*args, **kwargs)
     
     def postprocess(self, *args, **kwargs):
         return super().postprocess(*args, **kwargs)
+        
