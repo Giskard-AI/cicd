0bfec490cb8ac791cfa5e1bf7c7190333a532975
zcy <congyao119@gmail.com>
2024-01-23 21:19:44 +0100

    default value error handling

--- giskard_cicd/loaders/huggingface_inf_model.py
+++ giskard_cicd/loaders/huggingface_inf_model.py
@@ -68,15 +68,15 @@ def classification_model_from_inference_api(
                 sleep(0.5)
                 output = query(payload)
 
-            try:
-                for i in output:   
+            
+            for i in output:   
+                try:                   
                     sorted_output = sorted(i, key=lambda x: labels.index(x["label"]))
                     results.append([x["score"] for x in sorted_output])
-            except Exception as e:
-                logger.debug(f"Error: {e}")
-                logger.error(f"Unexpected format of output: {output}")
-                   
-                
+                except Exception as e:
+                    logger.debug(f"Error: {e}")
+                    logger.error(f"Unexpected format of output: {output}")
+                    results.append([-.1] * len(labels))
 
         logger.debug(f"Finished, got {len(results)} results")
 
